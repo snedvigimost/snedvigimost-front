c498d7de9583795a550034fc4f54364ede953a6a
a.oriekhov <a.oriekhov@spd-ukraine.com>
2020-11-29 16:35:56 +0200

    ready for backend translation

--- src/app/rest/districts/district.dto.ts
+++ src/app/rest/districts/district.dto.ts
@@ -4,11 +4,13 @@ export class DistrictDto {
   id: number;
   name: string;
   microDistricts: MicroDistrictDto[];
+  translations: any;
 
   constructor(data: DistrictDto) {
     this.id = data.id;
     this.name = data.name;
     this.microDistricts = data.microDistricts;
+    this.translations = data.translations;
   }
 
 }
